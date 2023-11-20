196e48aa193a6ffe3884c8957d99c705c9d0ba11
Sahil <sahilaktar648@gmail.com>
2023-11-20 15:00:33 +0530

    website for our Ideathon

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
+    const toggle = document.getElementById(toggleId);
     nav = document.getElementById(navId)
     
     if(toggle && nav){
@@ -9,28 +9,32 @@ const showMenu = (toggleId, navId) =>{
     }
 }
 showMenu('nav-toggle','nav-menu')
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav-link')
 
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu')  
-    navMenu.classList.remove('show-menu')
+    const navMenu = document.getElementById('nav-menu')  ;
+    navMenu.classList.remove('show-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 const sections = document.querySelectorAll('section[id]')
+
 function scrollActive(){
+
     const scrollY = window.pageYOffset
 
     sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
+        const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        sectionId = current.getAttribute('id');
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link');
         }
+
     })
 }
 window.addEventListener('scroll', scrollActive)
@@ -43,16 +47,21 @@ window.addEventListener('scroll', scrollHeader)
 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');    
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
 }
-window.addEventListener('scroll', scrollTop)
-const themeButton = document.getElementById('theme-button')
+window.addEventListener('scroll', scrollTop);
+const themeButton = document.getElementById('theme-button');
 const darkTheme = 'dark-theme'
+
 const iconTheme = 'bx-toggle-right'
 const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
+
+
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right';
 
 
 if (selectedTheme) {
@@ -68,24 +77,27 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
 const sr = ScrollReveal({
     distance: '30px',
     duration: 1800,
     reset: true,
 });
 
-sr.reveal(`.home__data, .home__img, 
-           .decoration__data,
-           .accessory__content,
-           .footer__content`, {
+
+sr.reveal(`.home-data, .home-img, 
+           .decoration-data,
+           .accessory-content,
+           .footer-content`, {
     origin: 'top',
     interval: 200,
 })
 
-sr.reveal(`.share__img, .send__content`, {
+sr.reveal(`.share-img, .send-content`, {
     origin: 'left'
 })
 
-sr.reveal(`.share__data, .send__img`, {
+sr.reveal(`.share-data, .send-img`, {
     origin: 'right'
 })
